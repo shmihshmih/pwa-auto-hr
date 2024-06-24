934a44c151cdbbe1a01b512d6f2f4df94c156484
biokashtanov <a.kashtanov@bioniq.com>
2024-06-24 16:53:10 +0300

    deploy starts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    component: MainPageComponent,
+    path: ''
+  },
+  {
+    component: MainPageComponent,
+    path: '/'
+  },
+  {
+    component: MainPageComponent,
+    path: 'main'
+  },
+  {
+    component: NotFoundComponent,
+    path: '404'
+  },
+  {
+    path: '**',
+    redirectTo: '404'
+  }
+];
